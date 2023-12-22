69ce8bdb87dfdad51d3c0e020b740d07ab419a26
vaibhavsraut <vaibhav.raut851@gmail.com>
2023-12-22 17:35:18 +0530

    Implemented StockAccount

---
+++ src/com/day11/StockAccount.java
@@ -0,0 +1,87 @@
+package com.day11;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Stock {
+    String stockName;
+    int numberOfShares;
+    double sharePrice;
+
+    public Stock(String stockName, int numberOfShares, double sharePrice) {
+        this.stockName = stockName;
+        this.numberOfShares = numberOfShares;
+        this.sharePrice = sharePrice;
+    }
+
+    double stockValue(){
+        return numberOfShares * sharePrice;
+    }
+}
+
+class StockPortfolio{
+    ArrayList<Stock> stocks;
+
+    public StockPortfolio() {
+        stocks = new ArrayList<>();
+    }
+
+    void addStock(String stockName, int numberOfShares, double sharePrice){
+        Stock stock = new Stock(stockName, numberOfShares, sharePrice);
+        stocks.add(stock);
+    }
+
+    double portfolioValue(){
+        double totalValue=0;
+        for(Stock stock: stocks) {
+            totalValue += stock.stockValue();
+        }
+        return totalValue;
+    }
+
+    void stockReport(){
+        System.out.println("Stock Report:");
+        System.out.println("******************");
+        for(Stock stock : stocks){
+            System.out.println("Stock name: " + stock.stockName);
+            System.out.println("Number of Shares: " + stock.numberOfShares);
+            System.out.println("Each ShareEN price $" + stock.sharePrice);
+            System.out.println("Stock Price: $"+ stock.stockValue());
+            System.out.println();
+        }
+        System.out.println("*******************************************************");
+        System.out.println("Portfolio Value: $"+ portfolioValue());
+    }
+}
+
+
+public class StockAccount {
+    static StockPortfolio stockPortfolio = new StockPortfolio();
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of stocks:");
+        int numberOfStocks = scanner.nextInt();
+
+        for (int i=1;i<=numberOfStocks;i++){
+            System.out.println("Enter details of stock number "+ i + ":");
+            System.out.print("Enter Stock Name: ");
+            String stockName = scanner.next();
+            System.out.print("Enter Number of Shares: ");
+            int numberOfShares = scanner.nextInt();
+            System.out.print("Enter Share Price: ");
+            double sharePrice = scanner.nextDouble();
+            System.out.println();
+
+            stockPortfolio.addStock(stockName, numberOfShares, sharePrice);
+        }
+
+        stockPortfolio.stockReport();
+
+
+    }
+}
+
+
